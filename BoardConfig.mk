135c112583bf0a5aa3e0421b85143fa3c50c15f9
hsufuchun <hsufuchun@gmail.com>
2013-01-31 02:19:34 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,16 +1,25 @@
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
 -include device/semc/mogami-common/BoardConfigCommon.mk
--include vendor/semc/smultron/BoardConfigVendor.mk
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/smultron/config
+
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_smultron_defconfig
 #TARGET_PREBUILT_KERNEL := device/semc/smultron/prebuilt/kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+# Sensor
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := false
 SENSORS_ACCEL_BMA250_INPUT := true
 SENSORS_PROXIMITY_APDS9700 := true
 SENSORS_PRESSURE_BMP180 := false
 
+# Assert
 TARGET_OTA_ASSERT_DEVICE := ST15i,ST15a,smultron
 
+# inherit from the proprietary version
+-include vendor/semc/smultron/BoardConfigVendor.mk
