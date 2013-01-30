8f072102a97ec3e742df9b5d2c696f6875460b08
hsufuchun <hsufuchun@gmail.com>
2013-01-31 02:25:05 +0800

    smultron: device specific part for the new two-stage boot method

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,8 @@ TARGET_KERNEL_CONFIG := cyanogen_smultron_defconfig
 #TARGET_PREBUILT_KERNEL := device/semc/smultron/prebuilt/kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+TARGET_RECOVERY_INITRC := device/semc/smultron/recovery/init.rc
+
 # Sensor
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := false
